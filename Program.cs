d1bfba70d2a851c8b3dcfdf2af575874600b799e
emilianiancu34@gmail.com <emilianiancu34@gmail.com>
2025-09-10 10:47:59 +0200

    created the IsEven method and the loop

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ovninigbranch
+﻿using System.Runtime.ExceptionServices;
+
+namespace ovninigbranch
 {
     internal class Program
     {
+        static bool IsEven(int a)
+        {
+            if (a % 2 == 0)
+                return true;
+            else return false;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers=new int [5];
+            for(int i=0;i<numbers.Length;i++)
+            {
+                IsEven(numbers[i]);
+            }
+
         }
     }
 }
